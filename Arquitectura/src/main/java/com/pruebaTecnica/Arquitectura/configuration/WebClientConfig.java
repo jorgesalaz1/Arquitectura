857c7373f071f8e32a1c478fdf02b26b45755825
jorgesalaz1 <jsalaza201@gmail.com>
2026-06-18 10:28:16 -0500

    fix: recrear WebClientConfig con mayusculas correctas para Linux

---
+++ Arquitectura/src/main/java/com/pruebaTecnica/Arquitectura/configuration/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.pruebatecnica.arquitectura.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@Configuration
+public class WebClientConfig {
+    public static final  String URL = "http://localhost:8080/api/v1/";
+    @Bean
+    public WebClient webClient(WebClient.Builder builder){
+        return builder
+                .baseUrl(URL)
+                .build();
+    }
+}
